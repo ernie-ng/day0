181bd1b699a38bf4c08670196fdb512baa63af98
Ernie N <ernie_ng@outlook.com>
2021-09-07 09:40:37 -0700

    Created project day0

---
+++ helloworld.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int x; //declaration
+
+	x = 7;	// initialization
+
+	int y = 5;  //declare and initial
+
+	int z;
+
+	z = x + y;
+
+	printf("Hello world! %d ", z );
+
+	return 0;
+}
